66eb725c378e868e4e9d3980878acda0248e8334
LukshaMikalai <lucshahomm@gmail.com>
2020-08-26 07:21:09 +0300

    upd 15

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,19 +1,17 @@
-create table applicant
+create table "user"
 (
-    id         serial not null
+    id       serial not null
         constraint user_pk
             primary key,
-    first_name varchar,
-    password   varchar,
-    email      varchar,
-    last_name  varchar
+    login    varchar,
+    password varchar
 );
 
-alter table applicant
+alter table "user"
     owner to postgres;
 
 create unique index user_id_uindex
-    on applicant (id);
+    on "user" (id);
 
 create table nationality
 (
@@ -47,5 +45,98 @@ alter table employer
 
 create unique index employer_id_uindex
     on employer (id);
+
+create table applicant
+(
+    id        serial not null
+        constraint applicant_pk
+            primary key,
+    firstname varchar,
+    lastname  varchar,
+    password  varchar,
+    email     varchar,
+    birthday  timestamp,
+    gender    varchar,
+    telephone varchar
+);
+
+alter table applicant
+    owner to postgres;
+
+create unique index applicant_id_uindex
+    on applicant (id);
+
+create table applicant_nationality
+(
+    applicant_id   integer,
+    nationality_id integer
+);
+
+alter table applicant_nationality
+    owner to postgres;
+
+create table profession
+(
+    id   serial not null
+        constraint profession_pk
+            primary key,
+    name varchar
+);
+
+alter table profession
+    owner to postgres;
+
+create unique index profession_id_uindex
+    on profession (id);
+
+create table language
+(
+    id   serial not null
+        constraint language_pk
+            primary key,
+    name varchar
+);
+
+alter table language
+    owner to postgres;
+
+create unique index language_id_uindex
+    on language (id);
+
+create table place_of_works
+(
+    id                         serial not null
+        constraint place_of_works_pk
+            primary key,
+    beginning_of_work          varchar,
+    end_of_work                varchar,
+    organization               varchar,
+    position_                  varchar,
+    workplace_responsibilities varchar
+);
+
+alter table place_of_works
+    owner to postgres;
+
+create unique index place_of_works_id_uindex
+    on place_of_works (id);
+
+create table education
+(
+    id                      serial not null
+        constraint education_pk
+            primary key,
+    level                   varchar,
+    educational_institution varchar,
+    faculty                 varchar,
+    specialization          varchar,
+    year_of_ending          integer
+);
+
+alter table education
+    owner to postgres;
+
+create unique index education_id_uindex
+    on education (id);
 
 
